05d0764d090a0555e6cc677898e1a7f990e080f6
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2025-03-13 00:08:39 +0530

    fix: correct cache found check

--- dist/index.js
+++ dist/index.js
@@ -30029,7 +30029,7 @@ async function setupGlobStar() {
 
         let downloadPath = '';
         downloadPath = tool_cache.find('globstar', version, getArch());
-        if (cacheOption || downloadPath) {
+        if (cacheOption & downloadPath !== '') {
             core.info(`Found cached Globstar binary at ${downloadPath}`);
         } else {
             core.info(`Downloading binary from ${downloadUrl}`);
